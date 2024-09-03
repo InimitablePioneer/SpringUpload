f3084247bf7b057c5ae85375d6879acf01e65049
InimitablePioneer <145961960+InimitablePioneer@users.noreply.github.com>
2024-09-03 23:11:45 +0900

    Sep 3 at home via MAC

--- src/main/java/hello/upload/controller/SpringUploadController.java
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 @Controller
 @RequestMapping("/spring")
@@ -24,11 +27,19 @@ public class SpringUploadController {
 
     @PostMapping("/upload")
     public String saveFile(@RequestParam String itemName,
-                           @RequestParam MultipartFile file, HttpServletRequest request) {
+                           @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
         log.info("request={}", request);
         log.info("itemName={}", itemName);
         log.info("multipartFile={}", file);
 
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
+
+        }
+
+        return "upload-form";
 
     }
 }
