e28dc9378c6aa1cd3a5a2f3a0b9fc8295bac943a
InimitablePioneer <seungwonpioneer@gmail.com>
2024-09-03 14:55:04 +0900

    Sep3 at work via WIN

---
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -0,0 +1,34 @@
+package hello.upload.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Controller
+@RequestMapping("/spring")
+public class SpringUploadController {
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam String itemName,
+                           @RequestParam MultipartFile file, HttpServletRequest request) {
+        log.info("request={}", request);
+        log.info("itemName={}", itemName);
+        log.info("multipartFile={}", file);
+
+
+    }
+}
